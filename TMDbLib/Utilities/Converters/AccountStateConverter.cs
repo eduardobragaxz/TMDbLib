2e1f70b9451912e6348169251d6d78b6ada14b12
Eduardo Braga <braga.eduardoTL@outlook.com>
2026-03-22 11:19:34 -0300

    Fix the Read override method in AccountStateConverter so it converts only the Rated property. Not sure what to do about the write override.

--- TMDbLib/Utilities/Converters/AccountStateConverter.cs
+++ TMDbLib/Utilities/Converters/AccountStateConverter.cs
@@ -9,12 +9,10 @@ using TMDbLib.Objects.TvShows;
 
 namespace TMDbLib.Utilities.Converters;
 
-internal class AccountStateConverter : JsonConverter<AccountState>
+internal class AccountStateConverter : JsonConverter<double?>
 {
-    public override AccountState? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override double? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var jObject = JsonObject.Create(JsonElement.ParseValue(ref reader));
-
         // Sometimes the AccountState.Rated is an object with a value in it
         // In these instances, convert it from:
         //  "rated": { "value": 5 }
@@ -23,53 +21,48 @@ internal class AccountStateConverter : JsonConverter<AccountState>
         //  "rating": 5
         //  "rating": null
 
-        var obj = jObject?["rated"]!;
         if (reader.TokenType == JsonTokenType.False)
         {
-            // It's "False", so the rating is not set
-            jObject?.Remove("rated");
-            jObject?.Add("rating", null);
+            return null;
         }
         else
-        // if (reader.TokenType == JsonTokenType.StartObject)
         {
-            // Read out the value
-            var rating = obj["value"]?.GetValue<double>();
-            jObject?.Remove("rated");
-            jObject?.Add("rating", rating);
+            JsonElement jElement = JsonElement.ParseValue(ref reader);
+
+            if (jElement.TryGetProperty("value", out JsonElement value) == false)
+            {
+                return null;
+            }
+
+            return value.GetDouble();
         }
-
-        using JsonDocument document = JsonDocument.Parse(jObject!.ToJsonString());
-        var result = document.RootElement.Deserialize(SourceGenerationContext.Default.AccountState);
-
-        return (AccountState?)result;
     }
 
-    public override void Write(Utf8JsonWriter writer, AccountState value, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, double? value, JsonSerializerOptions options)
     {
-        if (value is null)
-        {
-            writer.WriteNullValue();
-            return;
-        }
+        // if (value is null)
+        // {
+        //    writer.WriteNullValue();
+        //    return;
+        // }
 
-        var jsonObject = JsonSerializer.SerializeToNode(value, SourceGenerationContext.Default.AccountState)?.AsObject();
+        // var jsonObject = JsonSerializer.SerializeToNode(value, SourceGenerationContext.Default.AccountState)?.AsObject();
 
-        if (jsonObject != null)
-        {
-            var ratingNode = jsonObject["rating"];
-            jsonObject.Remove("rating");
+        // if (jsonObject != null)
+        // {
+        //    var ratingNode = jsonObject["rating"];
+        //    jsonObject.Remove("rating");
 
-            if (ratingNode is JsonValue jsonValue && jsonValue.TryGetValue<ContentRating>(out var contentRating) && contentRating is not null)
-            {
-                jsonObject["rated"] = JsonSerializer.SerializeToNode(new { value = contentRating }, SourceGenerationContext.Default.AccountState);
-            }
-            else
-            {
-                jsonObject["rated"] = null;
-            }
+        // if (ratingNode is JsonValue jsonValue && jsonValue.TryGetValue<ContentRating>(out var contentRating) && contentRating is not null)
+        //    {
+        //        jsonObject["rated"] = JsonSerializer.SerializeToNode(new { value = contentRating }, SourceGenerationContext.Default.AccountState);
+        //    }
+        //    else
+        //    {
+        //        jsonObject["rated"] = null;
+        //    }
 
-            jsonObject.WriteTo(writer);
-        }
+        // jsonObject.WriteTo(writer);
+        // }
     }
 }
